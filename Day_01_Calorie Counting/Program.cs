84532084a2d1fa56e945b0155d4267c8376e0dab
Carlos Vilorio <vilorio@outlook.com>
2022-12-01 14:02:04 -0400

    Primera solucion

--- Day_01_Calorie Counting/Program.cs
+++ Day_01_Calorie Counting/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+using System.Diagnostics;
 
-Do("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000");
+string data = File.ReadAllText("Data\\entrada1.txt");
+
+//Do("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000");
+Stopwatch sw = Stopwatch.StartNew();
+DateTime start = DateTime.Now;
+Do(data.Replace("\r",""));
+sw.Stop();
+Console.WriteLine($"Time in Milliseconds: {sw.ElapsedMilliseconds}");
 
 void Do(string entrada)
 {
