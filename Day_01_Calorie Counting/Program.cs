77dcdf4de5c76e56ae7d919058c1185d1d3c1080
Carlos Vilorio <vilorio@outlook.com>
2022-12-02 17:46:04 -0400

    refact day one

--- Day_01_Calorie Counting/Program.cs
+++ Day_01_Calorie Counting/Program.cs
@@ -1,43 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
 
-using System.Diagnostics;
+Stopwatch sw = Stopwatch.StartNew();
+
 
 string data = File.ReadAllText("Data\\entrada1.txt");
-
-//Do("1000,2000,3000,,4000,,5000,6000,,7000,8000,9000,,10000");
-Stopwatch sw = Stopwatch.StartNew();
-DateTime start = DateTime.Now;
+//Do("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000");
 Do(data.Replace("\r",""));
+
 sw.Stop();
+
 Console.WriteLine($"Time in Milliseconds: {sw.ElapsedMilliseconds}");
 
+Console.ReadKey();
 void Do(string entrada)
 {
-    //string entry = "";
-    int max = 0;
     int actual = 0;
+    var xdata = entrada.Split('\n')
+        .ToList()
+        .Select((v, i) => new { Value = v, Index = i });
 
-    foreach (var entry in entrada.Split(new[] { ',','\n'}))
+    List<int> maxList = new();
+
+    int count  = xdata.Count();
+
+    foreach (var entry in xdata)
     {
-        if (entry != "")
+        if (entry.Value != "")
         {
-            actual += int.Parse(entry.Trim());
-            if (actual > max)
-            {
-                max = actual;
-            }
+            actual += int.Parse(entry.Value.Trim());
         }
-        else
-        {
+
+        if(entry.Index + 1 == count || entry.Value == "")
+        { 
+            maxList.Add(actual);
             actual = 0;
         }
-
     }
 
-    Console.WriteLine($"La sumatoria del reno con mayor calorias es: {max}");  
+    var top3 = maxList
+        .OrderByDescending(p => p)
+        .Take(3)
+        .ToList();
+
+    Console.WriteLine($"La sumatoria del reno con mayor calorías es: {top3[0]}");
+    Console.WriteLine($"La sumatoria de los tres renos con mayor calorías es es: {top3.Sum()}");
 }
-
-
-
-
